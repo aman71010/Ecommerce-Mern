2e8e8ae81938c7d42bd5dbc0421759d284f92f79
Aman <amangupta71010@gmail.com>
2021-10-29 21:38:15 +0530

    handle user validation, completed register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 import { tablet2, mobile2 } from '../responsive';
+import { useState, useEffect  } from 'react';
+import { publicRequest } from '../requestMethods.js';
+import { useHistory } from 'react-router';
 
 const Container = styled.div`
     width: 100vw;
@@ -35,12 +38,22 @@ const Form = styled.form`
     ${mobile2({flexWrap: "nowrap", flexDirection: "column"})};
 `;
 
-const Input = styled.input`
+const InputDiv = styled.div`
     flex: 1;
     min-width: 40%;
     margin: 20px 10px 0 0;
+    ${mobile2({margin: "10px 0 0 0"})};
+`;
+
+const Input = styled.input`
+    width: 90%;
     padding: 10px;
-    ${mobile2({margin: "10px 0 0 0", padding: "5px"})};
+    ${mobile2({padding: "5px"})};
+`;
+
+const ErrorMessage = styled.p`
+    color: red;
+    font-size: 14px;
 `;
 
 const Agreement = styled.span`
@@ -59,22 +72,151 @@ const Button = styled.button`
 `;
 
 const Register = () => {
+
+    const history = useHistory();
+    const [formValues, setFormValues] = useState({
+        firstName: '',
+        lastName: '',
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
+
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
+
+    const inputHandler = (e) => {
+        setFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setFormErrors(validate(formValues));
+        setIsSubmit(true);
+    };
+
+    useEffect( () => {
+
+        const register = async () => {
+            if(Object.keys(formErrors).length === 0 && isSubmit === true){
+                try{
+                    const res = await publicRequest.post("/auth/register", formValues);
+                    if(res){
+                        history.push("/login");
+                    }
+                } catch(err){
+                    console.log(err);
+                }
+            }
+        };
+        register();
+    }, [formErrors, isSubmit]);
+
+    const validate = (values) => {
+        const errors = {};
+        const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if(!values.firstName){
+            errors.firstName = "First Name is required";
+        }
+        if(!values.lastName){
+            errors.lastName = "Last Name is required";
+        }
+        if(!values.username){
+            errors.username = "Username is required";
+        } else if(values.username.length < 5) {
+            errors.username = "Username must be more than 4 characters";
+        }
+        if(!values.email){
+            errors.email = "Email is required";
+        } else if(!regex.test(values.email)){
+            errors.email = "Email is not valid";
+        }
+        if(!values.password){
+            errors.password = "Password is required";
+        }else if(values.password.length < 4){
+            errors.password = "Password must be more than 3 characters";
+        }else if(values.password.length > 10){
+            errors.password = "Password must be not more than 10 characters";
+        }
+        if(!values.confirmPassword){
+            errors.confirmPassword = "Confirm Password is required";
+        }else if(values.password !== values.confirmPassword){
+            errors.confirmPassword = "Both the passwords must be same";
+        }
+
+        return errors;
+    };
+
     return (
         <Container>
             <Wrapper>
                 <Title>CREATE AN ACCOUNT</Title>
-                <Form>
-                    <Input placeholder="first name" />
-                    <Input placeholder="last name" />
-                    <Input placeholder="username" />
-                    <Input placeholder="email" />
-                    <Input placeholder="password" />
-                    <Input placeholder="confirm password" />
+                <Form onSubmit={handleSubmit}>
+                    <InputDiv>
+                        <Input 
+                            placeholder="first name" 
+                            name="firstName" 
+                            value={formValues.firstName} 
+                            onChange={inputHandler}
+                        />
+                        <ErrorMessage>{formErrors.firstName}</ErrorMessage>
+                    </InputDiv>
+                    <InputDiv>
+                        <Input 
+                            placeholder="last name" 
+                            name="lastName" 
+                            value={formValues.lastName} 
+                            onChange={inputHandler}  
+                        />
+                        <ErrorMessage>{formErrors.lastName}</ErrorMessage>
+                    </InputDiv>   
+                    <InputDiv>
+                        <Input 
+                            placeholder="username" 
+                            name="username" 
+                            value={formValues.username} 
+                            onChange={inputHandler}
+                        />
+                        <ErrorMessage>{formErrors.username}</ErrorMessage>
+                    </InputDiv>
+                    <InputDiv>
+                        <Input 
+                            placeholder="email"
+                            name="email" 
+                            value={formValues.email}
+                            onChange={inputHandler}
+                        />
+                        <ErrorMessage>{formErrors.email}</ErrorMessage>
+                    </InputDiv>
+                    <InputDiv>
+                        <Input 
+                            type="password"
+                            placeholder="password" 
+                            name="password" 
+                            value={formValues.password} 
+                            onChange={inputHandler}
+                        />
+                        <ErrorMessage>{formErrors.password}</ErrorMessage>
+                    </InputDiv>
+                    <InputDiv>
+                        <Input 
+                            type="password"
+                            placeholder="confirm password" 
+                            name="confirmPassword" 
+                            value={formValues.confirmPassword} 
+                            onChange={inputHandler}                      
+                        />
+                        <ErrorMessage>{formErrors.confirmPassword}</ErrorMessage>
+                    </InputDiv> 
                     <Agreement>
                         By creating an account, I consent to the processing of my personal
                         data in accordance with the <b>PRIVACY POLICY</b>
                     </Agreement>
-                    <Button>CREATE</Button>
+                    <Button type="submit">CREATE</Button>
                 </Form>
             </Wrapper>
         </Container>
