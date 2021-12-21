b4a0fbe8f66213e19bb1f5380d68d109cc9fb711
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2021-12-21 13:38:46 +0530

    updated requestMethod

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:8080/api"
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNjEzNTkxM2E4NWRjZmZjZDc2MzE3NCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzMzc2MDcwOSwiZXhwIjoxNjM0MDE5OTA5fQ.IKSg_f7aWAlo5NzycdMk1hvuHTxQhA4Ft7wLFEKsUX4";
+const TOKEN = JSON.parse(localStorage.getItem("profile"))?.accessToken;
 
 export const publicRequest = axios.create({
     baseURL:  BASE_URL
